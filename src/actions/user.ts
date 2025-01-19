d4876fef6961c520ab70efd9d1eb9d2b60a1f2a3
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-01-19 21:38:34 +0530

    Prisma  Schema

---
+++ src/actions/user.ts
@@ -0,0 +1,18 @@
+"use sever";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+export const onAuthenticateUser = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return { status: 403 };
+    }
+
+    // const existingUser = await db.user.findUnique({
+    //   where: {
+    //     clerkId: user.id,
+    //   },
+    // });
+  } catch (error) {}
+};
