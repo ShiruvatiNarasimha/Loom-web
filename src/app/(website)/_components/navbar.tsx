ca77c9384e63b0565059eacb6535a1e550fac69b
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-01-19 20:18:33 +0530

    Updated Navbar

--- src/app/(website)/_components/navbar.tsx
+++ src/app/(website)/_components/navbar.tsx
@@ -1,5 +1,7 @@
-import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Menu, User } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -8,12 +10,28 @@ const LandingPageNavBar = (props: Props) => {
   return (
     <div className="flex w-full justify-between items-center">
       <div className="text-3xl font-semibold flex items-center gap-x-3">
-        <Menu className="w-6 h-6" />
-        <Image alt="logo" src="/loom-logo.svg" width={80} height={80} />
+        <Menu className="w-8 h-8" />
+        <Image alt="logo" src="/logo.svg" width={40} height={40} />
+        Web 2.0
       </div>
-      LandingPageNavBar
+      <div className="hidden gap-x-10 items-center lg:flex">
+        <Link
+          href="/"
+          className="bg-[#7320DD] py-2 px-5 font-semibold 
+        text-lg rounded-full hover:bg-[#7320DD]/80"
+        >
+          Home
+        </Link>
+        <Link href="/">Pricing</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      <Link href="/auth/sign-in">
+        <Button className=" text-base flex gap-x-2 ">
+          <User fill="#000" />
+          Login
+        </Button>
+      </Link>
     </div>
   );
 };
-
 export default LandingPageNavBar;
